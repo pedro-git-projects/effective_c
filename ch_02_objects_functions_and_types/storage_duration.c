fd61335f56529259b7e9245a5b6d8d0fb4c698d2
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-12-20 10:57:08 -0300

    storage duration updated

--- ch_02_objects_functions_and_types/storage_duration.c
+++ ch_02_objects_functions_and_types/storage_duration.c
@@ -24,3 +24,29 @@ int main(void) {
 		nonStaticIncrement();
 	}
 }
+
+/* Objects have a storage duration that determines their lifetime.
+ * There are four storage durations:
+ * automatic, static, thread and allocated.
+ * Objects in file scope have static storage duration.
+ * We can also declare a variable within block scope and use the
+ * storage class specifier static to make it static.
+ * A static variable will live trough the whole execution of the
+ * program and will mantain its las stored value troughout its lifetime
+ * */
+
+/* Static objects must be initialized with a constant value, not a variable
+ * constant values are such as literal constants, like 1, 'a' and 0xFF
+ * enum members
+ * and the results of operators like alignof sizeof
+ * const qualified objects don't make the cut, so 
+ *  const int j = i;
+ *  static int k = j; 
+ *  will error out
+ * */
+
+/* thread storage duration is outside the scope of this material
+ *  allocated deals with dynamically allocated memory and 
+ *  will be discussed later
+ * */
+
